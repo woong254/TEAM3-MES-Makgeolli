dcd6a39eb101c0d5451403c6a0c837de2353f124
woong <wjdwldnd254@naver.com>
2025-09-30 10:23:03 +0900

    정지웅/발주메뉴하위메뉴작성/25.09.30

--- client/src/views/Department/materials/PurManagement.vue
+++ client/src/views/Department/materials/PurManagement.vue
@@ -2,16 +2,17 @@
   <AdminLayout>
     <PageBreadcrumb :pageTitle="currentPageTitle" />
     <div class="space-y-5 sm:space-y-6">
-      <ComponentCard title="Bar Chart 1"> <BarChartOne /> </ComponentCard>
+      <VueGoodTable title="Bar Chart 1"> <BarChartOne /> </VueGoodTable>
     </div>
   </AdminLayout>
 </template>
 
 <script setup>
 import BarChartOne from '@/components/charts/BarChart/BarChartOne.vue'
-import ComponentCard from '@/components/common/ComponentCard.vue'
+import 'vue-good-table/dist/vue-good-table.css'
+import { VueGoodTable } from 'vue-good-table'
 import PageBreadcrumb from '@/components/common/PageBreadcrumb.vue'
 import AdminLayout from '@/components/layout/AdminLayout.vue'
 import { ref } from 'vue'
-const currentPageTitle = ref('Bar Chart')
+const currentPageTitle = ref('입고관리')
 </script>
