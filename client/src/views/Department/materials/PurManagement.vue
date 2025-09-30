56cbf3638c84c3f4e8158a36cf60183bf2eac41e
woong254 <wjdwldnd254@naver.com>
2025-10-01 01:05:29 +0900

    정지웅/primvue적용/25.10.01

--- client/src/views/Department/materials/PurManagement.vue
+++ client/src/views/Department/materials/PurManagement.vue
@@ -1,9 +1,6 @@
 <script setup>
-import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community'
-ModuleRegistry.registerModules([AllCommunityModule])
-import { AgGridVue } from 'ag-grid-vue3'
-import 'ag-grid-community/styles/ag-grid.css'
-import 'ag-grid-community/styles/ag-theme-alpine.css'
+import DataTable from 'primevue/datatable'
+import DataCol from 'primevue/column'
 import ComponentCard from '@/components/common/ComponentCard.vue'
 import PageBreadcrumb from '@/components/common/PageBreadcrumb.vue'
 import AdminLayout from '@/components/layout/AdminLayout.vue'
@@ -12,18 +9,7 @@ import { ref } from 'vue'
 
 const currentPageTitle = ref('발주관리')
 
-const colDefs = ref([
-  { field: 'pur_code', flex: 1 },
-  { field: 'pur_name', flex: 1 },
-  { field: 'bcnc_code', flex: 1 },
-  { field: 'bcnc_name', flex: 1 },
-  { field: 'pur_date', flex: 1 },
-  { field: 'receipt_date', flex: 1 },
-  { field: 'emp_name', flex: 1 },
-  { field: 'remark', flex: 1 },
-])
-
-const rowData = ref([
+const purChase = ref([
   {
     pur_code: 'BAL-001',
     pur_name: '20250930쌀발주',
@@ -34,7 +20,18 @@ const rowData = ref([
     emp_name: '정지웅',
     remark: '쌀 많이 주세요~~',
   },
+  {
+    pur_code: 'BAL-002',
+    pur_name: '20251001보리발주',
+    bcnc_code: 2002,
+    bcnc_name: '보리상사',
+    pur_date: '2025-10-01',
+    receipt_date: '2025-10-31',
+    emp_name: '김지민',
+    remark: '보리 조금만 주세요~',
+  },
 ])
+const selectedRows = ref([])
 </script>
 
 <template>
@@ -48,13 +45,54 @@ const rowData = ref([
           <Button size="sm" variant="primary"> 등록/수정 </Button>
           <Button size="sm" variant="primary"> 삭제 </Button>
         </div>
-        <AgGridVue
-          class="ag-theme-alpine"
-          domLayout="autoheight"
-          :columnDefs="colDefs"
-          :rowData="rowData"
-          width="100%"
-        />
+        <DataTable
+          :value="purChase"
+          v-model:selection="selectedRows"
+          showGridlines
+          selectionMode="multiple"
+          dataKey="pur_code"
+        >
+          <DataCol selectionMode="multiple" headerStyle="width: 3rem" />
+          <DataCol
+            field="pur_code"
+            header="발주코드"
+            :pt="{ columnHeaderContent: 'justify-center' }"
+          />
+          <DataCol
+            field="pur_name"
+            header="발주명"
+            :pt="{ columnHeaderContent: 'justify-center' }"
+          />
+          <DataCol
+            field="bcnc_code"
+            header="공급업체코드"
+            style="text-align: right"
+            :pt="{ columnHeaderContent: 'justify-center' }"
+          />
+          <DataCol
+            field="bcnc_name"
+            header="공급업체명"
+            :pt="{ columnHeaderContent: 'justify-center' }"
+          />
+          <DataCol
+            field="pur_date"
+            header="발주일자"
+            style="text-align: center"
+            :pt="{ columnHeaderContent: 'justify-center' }"
+          />
+          <DataCol
+            field="receipt_date"
+            header="입고일자"
+            style="text-align: center"
+            :pt="{ columnHeaderContent: 'justify-center' }"
+          />
+          <DataCol
+            field="emp_name"
+            header="담당자"
+            :pt="{ columnHeaderContent: 'justify-center' }"
+          />
+          <DataCol field="remark" header="비고" :pt="{ columnHeaderContent: 'justify-center' }" />
+        </DataTable>
       </ComponentCard>
     </div>
   </AdminLayout>
