2d3ab49a5b793cda4023d863d2de3d4175cb9839
bong <p1-02@naver.com>
2025-10-01 13:12:17 +0900

    박봉근/크롬 다크모드에서 글자 색 변경/25.10.01

--- client/src/views/Department/production/prodOrd.vue
+++ client/src/views/Department/production/prodOrd.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-import AdminLayout from '@/components/layout/AdminLayout.vue'
-import PageBreadcrumb from '@/components/common/PageBreadcrumb.vue'
+import AdminLayout from '@/components/layout/AdminLayout.vue';
+import PageBreadcrumb from '@/components/common/PageBreadcrumb.vue';
 import ComponentCard from '@/components/common/ComponentCard.vue';
-import Button from '@/components/ui/Button.vue'
+import '@/assets/common.css'
+import { ref } from 'vue'
 
 const currentPageTitle = ref('생산지시관리');
 </script>
@@ -10,22 +11,27 @@ const currentPageTitle = ref('생산지시관리');
 <template>
   <AdminLayout>
     <PageBreadcrumb :pageTitle="currentPageTitle" />
-    <!-- <div class="space-y-5 sm:space-y-6">
-      <ComponentCard title="발주서">
-        <div class="flex justify-end space-x-2 mb-2">
-          <Button size="sm" variant="outline"> 초기화 </Button>
-          <Button size="sm" variant="outline"> 조회 </Button>
-          <Button size="sm" variant="primary"> 등록/수정 </Button>
-          <Button size="sm" variant="primary"> 삭제 </Button>
-        </div>
-        <AgGridVue
-          class="ag-theme-alpine"
-          domLayout="autoheight"
-          :columnDefs="colDefs"
-          :rowData="rowData"
-          width="100%"
-        />
+
+    <div class="space-y-5 sm:space-y-6">
+      <ComponentCard
+        title="기본사항"
+        :class="[
+          'shadow-md rounded-2xl border border-gray-200 bg-white dark:border-gray-800 dark:bg-white/[0.03]',
+        ]"
+      >
+        <template #header>
+          <div class="flex justify-end space-x-2 mb-2">
+            <button type="button" class="btn-white btn-common">초기화</button>
+            <button type="button" class="btn-white btn-common">조회</button>
+            <button type="button" class="btn-color btn-common">등록/수정</button>
+            <button type="button" class="btn-color btn-common">삭제</button>
+          </div>
+        </template>
+        <template #body>
+
+        </template>
       </ComponentCard>
-    </div> -->
+    </div>
+    
   </AdminLayout>
 </template>
