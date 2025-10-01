2a26a08373106c2e9cb7a2ecae4cc966bba84f0b
HANSOL LEE <hansol2886@gmail.com>
2025-10-01 16:52:54 +0900

    이한솔/품질기준관리-폼형태-수정/25.10.01

--- client/src/views/Department/quality/QualityMaster.vue
+++ client/src/views/Department/quality/QualityMaster.vue
@@ -24,7 +24,7 @@ const textareaStyle =
   'dark:bg-dark-900 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-950 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800'
 
 // table data
-const inspData = ([ref
+const inspData = ref([
   {
     inspCode: 'QC0001',
     inspName: '쌀 외관검사',
@@ -63,21 +63,6 @@ const inspTarget = ref([
 
 // table radio button
 const selectedInspData = ref(null)
-
-// 타입(예시)
-type inspTarget = {
-  proID: string
-  proName: string
-  proType: string
-  proStrand: string
-  proUnit: string
-}
-
-const deleteRow = (row: inspTarget) => {
-  console.log('삭제:', row)
-  // 예시 삭제 로직
-  // inspData.value = inspData.value.filter(r => r.inspCode !== row.inspCode)
-}
 </script>
 
 <template>
