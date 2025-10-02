2f5462eb8f843e17d566069a3e6055da549cb7d0
HANSOL LEE <hansol2886@gmail.com>
2025-10-02 17:13:11 +0900

    이한솔/품질기준관리-버튼및텍스트에리아수정/25.10.02

--- client/src/views/Department/quality/InspTargetSelectModal.vue
+++ client/src/views/Department/quality/InspTargetSelectModal.vue
@@ -113,8 +113,8 @@ const inspData = ref([
     <Modal title="검사대상" :fullScreenBackdrop="true">
       <template #modal-header>
         <div class="flex justify-end">
-          <button class="btn-common-modal btn-white">초기화</button>
-          <button class="btn-common-modal btn-color">조회</button>
+          <button type="button" class="btn-common-modal btn-white">초기화</button>
+          <button type="button" class="btn-common-modal btn-color">조회</button>
         </div>
       </template>
       <template #modal-body>
