0bf0b3455f9b5d4a0459cc5fd23411679727d902
hansoring <hansol2886@gmail.com>
2025-10-05 13:58:06 +0900

    이한솔/검사대상조회/2025.10.05

--- server/routers/quality_router.js
+++ server/routers/quality_router.js
@@ -5,4 +5,17 @@ const router = express.Router();
 
 const qualityService = require('../services/quality_service.js');
 
+// 모달: 검사대상 조회
+router.get('/inspTarget', async(req, res)=> {
+  // 나중에 로그인 기능 구현 후 변경예정
+  // let empId = req.body.empId;
+  // let empId = 'EMP-20250616-0002';
+
+  let inspTargetList = await qualityService
+    .findInspTarget()
+    .catch(err => console.error(err));
+    
+  res.send(inspTargetList);
+});
+
 module.exports = router;
