d8671d595386d4418e87fb9fde4c8bc64afe1c94
방재우 <wodn2365@gmail.com>
2025-10-07 03:57:03 +0900

    방재우/설비-관리-서버/1006

--- server/routers/equipment_router.js
+++ server/routers/equipment_router.js
@@ -5,6 +5,16 @@ const router = express.Router();
 
 const equipmentService = require("../services/equipment_service.js");
 
-router.get("/equipForm", async (req, resp) => {});
+//전체 조회
+router.get("/equipment", async (req, res) => {
+  try {
+    const params = req.query;
+    const equipList = await equipmentService.findAll(params);
+    res.send(equipList);
+  } catch (err) {
+    console.error("DB 조회 실패:", err);
+    res.status(500).send({ message: "서버 조회 중 오류 발생" });
+  }
+});
 
 module.exports = router;
