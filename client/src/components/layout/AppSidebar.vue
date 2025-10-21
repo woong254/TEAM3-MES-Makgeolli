9431384c7ddbaa741047b2222518ef3be105d400
woong <wjdwldnd254@naver.com>
2025-10-21 16:07:53 +0900

    정지웅/설비 관리 수정/25.10.21

--- client/src/components/layout/AppSidebar.vue
+++ client/src/components/layout/AppSidebar.vue
@@ -283,8 +283,7 @@ const menuGroups: MenuGroup[] = [
         name: '설비',
         subItems: [
           { name: '설비관리', path: '/equipmaster', pro: false },
-          { name: '비가동 설비조회', path: '/downtimelist', pro: false },
-          { name: '비가동 설비관리', path: '/downtimemanage', pro: false },
+          { name: '비가동 설비관리', path: '/downtimelist', pro: false },
           { name: '점검관리', path: '/', pro: false },
           { name: '점검항목조회', path: '/', pro: false },
           { name: '수리관리', path: '/Signin', pro: false },
